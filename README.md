2b2612f2ba61b66d063e9579c600ece998f0222e
Divy Srivastava <dj.srivastava23@gmail.com>
2021-08-24 16:17:45 +0530

    font rendering fixes (#1)

--- README.md
+++ README.md
@@ -1,29 +1,16 @@
+`deno_flappy`
 
 # flappybird_deno ![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)
 
-flappybird_deno is a clone of Flappy bird written in
-deno and sdl2
+flappybird clone of written in
+[deno_sdl2](https://github.com/littledivy/deno_sdl2)
 
 ## Installation
 
-
-1) Clone the repository
 ```shell
-git clone https://github.com/dhairy-online/flappybird.git
+$ git clone https://github.com/dhairy-online/flappybird.git
+$ deno compile -A --unstable main.js
+
+$ ./flappybird
 ```
-2) Download the binary file from releases
 
-- [ ]  In progress...
-    
-## Features
-
-- High Performance
-- Cross platform
-
-  
-## Authors
-
-- [@dhairy-online](https://www.github.com/dhairy-online)
-- [@littledivy](https://www.github.com/littledivy)
-
-  
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import { Canvas } from "https://deno.land/x/sdl2@0.1-alpha.5/src/canvas.ts";
 
 const canvas = new Canvas({
-  title: "deno_flappy_bird 🐦",
-  height: 800,
-  width: 600,
+  title: "Flappy Bird in Deno 🐦",
+  height: 400,
+  width: 800,
   centered: true,
   fullscreen: false,
   hidden: false,
@@ -38,10 +38,8 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-const width = Math.round(770 / 20) * 5;
-const height = Math.round(169 / 20) * 5;
 
-let playerX = 370;
+let playerX = 170;
 let playerY = 100;
 let is_space = false;
 
@@ -61,19 +59,19 @@ let x_font = 0, y_font = 0;
 let gameOver = false;
 let intro = true;
 
-upperPipes.push({ x: 800 + PIPE_WIDTH, height: getRandomInt(100, 200) });
+upperPipes.push({ x: 400 + PIPE_WIDTH, height: getRandomInt(100, 200) });
 upperPipes.push({
-  x: 800 + (PIPE_WIDTH * 2) + PIPE_DISTANCE,
+  x: 400 + (PIPE_WIDTH * 2) + PIPE_DISTANCE,
   height: getRandomInt(100, 200),
 });
 
 // Screen width - Corresponding upper pipe height - Random Gap
 lowerPipes.push({
-  x: 800 + PIPE_WIDTH,
+  x: 400 + PIPE_WIDTH,
   height: 800 - upperPipes[0].height - GAP,
 });
 lowerPipes.push({
-  x: 800 + (PIPE_WIDTH * 2) + PIPE_DISTANCE,
+  x: 400 + (PIPE_WIDTH * 2) + PIPE_DISTANCE,
   height: 800 - upperPipes[1].height - GAP,
 });
 
@@ -89,9 +87,6 @@ const birdSurfaceDownflap = canvas.loadSurface(
 const birdTextureDownflap = canvas.createTextureFromSurface(
   birdSurfaceDownflap,
 );
-
-const startScreenSurface = canvas.loadSurface("images/start.png");
-const startScreenTexture = canvas.createTextureFromSurface(startScreenSurface);
 
 const BgScreenSurface = canvas.loadSurface("images/background.png");
 const BgScreenTexture = canvas.createTextureFromSurface(BgScreenSurface);
@@ -112,75 +107,21 @@ let animationCycle = 0; // 0, 1, 2
 let prevTime = performance.now();
 
 canvas.on("draw", () => {
+  if (intro) {
+    return;
+  }
+
   const currTime = performance.now();
   const deltaTime = currTime - prevTime;
   prevTime = currTime;
-  if (intro) {
-    canvas.clear();
-    canvas.copy(startScreenTexture, { x: 0, y: 0, width: 800, height: 600 }, {
-      x: 0,
-      y: 0,
-      width: 800,
-      height: 600,
-    });
-    canvas.renderFont(font, "Press Space", {
-      blended: { color: { r: 209, g: 27, b: 20, a: 255 } },
-    }, {
-      x: Math.floor(x_font) + 300,
-      y: Math.floor(y_font) + 470,
-      width,
-      height,
-    });
-    canvas.renderFont(font, "  to start ", {
-      blended: { color: { r: 209, g: 27, b: 20, a: 255 } },
-    }, {
-      x: Math.floor(x_font) + 290,
-      y: Math.floor(y_font) + 520,
-      width,
-      height,
-    });
 
-    canvas.present();
-    return;
-  }
-  if (gameOver) {
-    canvas.renderFont(font, "Game Over!", {
-      blended: { color: { r: 209, g: 27, b: 20, a: 255 } },
-    }, {
-      x: Math.floor(x_font) + 64,
-      y: Math.floor(y_font) + 64,
-      width,
-      height,
-    });
-    canvas.present();
-    return;
-  }
-  canvas.setDrawColor(255, 255, 90, 255);
-
-  canvas.clear();
-  canvas.copy(BgScreenTexture, { x: 0, y: 0, width: 800, height: 600 }, {
+  canvas.copy(BgScreenTexture, { x: 0, y: 0, width: 400, height: 800 }, {
     x: 0,
     y: 0,
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 800,
   });
-  // canvas.fillRect(playerX, playerY, 50, 50);
-  animationCycle += 1;
-  if (animationCycle >= 3) {
-    animationCycle = 0;
-  }
-  canvas.copy(birdTextures[animationCycle], {
-    x: 0,
-    y: 0,
-    width: 34,
-    height: 24,
-  }, {
-    x: playerX,
-    y: playerY,
-    width: 34,
-    height: 24,
-  });
-  canvas.setDrawColor(181, 14, 26, 255);
+
   for (let idx = 0; idx < upperPipes.length; idx++) {
     if (
       checkCollision(
@@ -204,12 +145,14 @@ canvas.on("draw", () => {
         lowerPipes[idx].height,
       )
     ) {
-      // TODO: Hit (complete)
-      gameOver = true;
-      let score_effects = ["game_over.wav", "game_over_2.wav"];
-      canvas.playMusic(
-        "./audio/" + score_effects[Math.floor(Math.random() * 2)],
-      );
+      // Only runs once
+      if (!gameOver) {
+        gameOver = true;
+        canvas.playMusic(
+          "./audio/game_over.wav",
+        );
+        canvas.present();
+      }
     }
     if (
       checkCollision(
@@ -223,7 +166,6 @@ canvas.on("draw", () => {
         800 - upperPipes[idx].height - lowerPipes[idx].height,
       )
     ) {
-      // Make Score + 1 when pipe crossed
       score_value += 1;
       let score_effects = ["scored_1.wav", "scored_2.wav"];
       canvas.playMusic(
@@ -234,7 +176,7 @@ canvas.on("draw", () => {
     // Debug:
     // canvas.fillRect(playerX + 50 / 2, playerY, 0, 50)
     // canvas.fillRect(upperPipes[idx].x + PIPE_WIDTH / 2, upperPipes[idx].height, 0, 800 - upperPipes[idx].height - lowerPipes[idx].height);
-    
+
     // Pipes
     canvas.copy(pipeTextureDown, { x: 0, y: 0, width: 52, height: 320 }, {
       x: upperPipes[idx].x,
@@ -248,36 +190,61 @@ canvas.on("draw", () => {
       width: PIPE_WIDTH,
       height: lowerPipes[idx].height,
     });
+    canvas.copy(birdTextures[animationCycle], {
+      x: 0,
+      y: 0,
+      width: 34,
+      height: 24,
+    }, {
+      x: playerX,
+      y: playerY,
+      width: 34,
+      height: 24,
+    });
+    if (!gameOver) {
+      // Wing animation
+      animationCycle += 1;
+      if (animationCycle >= 3) {
+        animationCycle = 0;
+      }
 
-    upperPipes[idx].x -= 1;
-    lowerPipes[idx].x -= 1;
-    if (upperPipes[idx].x <= -PIPE_WIDTH) {
-      upperPipes[idx].x = 800 + PIPE_WIDTH;
-      upperPipes[idx].height = getRandomInt(100, 200);
-      lowerPipes[idx].x = 800 + PIPE_WIDTH;
-      lowerPipes[idx].height = 800 - upperPipes[idx].height - GAP;
+      upperPipes[idx].x -= 1;
+      lowerPipes[idx].x -= 1;
+      if (upperPipes[idx].x <= -PIPE_WIDTH) {
+        upperPipes[idx].x = 800 + PIPE_WIDTH;
+        upperPipes[idx].height = getRandomInt(100, 200);
+        lowerPipes[idx].x = 800 + PIPE_WIDTH;
+        lowerPipes[idx].height = 800 - upperPipes[idx].height - GAP;
+      }
+
+      if (playerY >= 800 - 50) {
+        gameOver = true;
+
+        canvas.playMusic(
+          "./audio/game_over.wav",
+        );
+      }
     }
-  }
-  canvas.renderFont(font, "Score: " + score_value, {
-    blended: { color: { r: 127, g: 201, b: 201, a: 255 } },
-  }, {
-    x: Math.floor(x_font) + 550,
-    y: Math.floor(y_font) + 550,
-    width,
-    height,
-  });
-  if (is_space) {
-    playerY -= 2;
-    setTimeout(() => is_space = false, 84);
-  } else {
-    // Give player gravity downwards
-    playerY += gravity;
-  }
-  if (playerY >= 600 - 50) {
-        gameOver = true
+    canvas.renderFont(font, score_value.toString(), {
+      blended: { color: { r: 255, g: 255, b: 255, a: 255 } },
+    }, {
+      x: 10,
+      y: -20,
+    });
+    if (is_space) {
+      playerY -= 50;
+      is_space = false;
+    } else {
+      // Give player gravity downwards
+      playerY += gravity;
+    }
+    if (playerY >= 800 - 50) {
+      playerY = 800 - 50;
+    }
   }
 
   canvas.present();
+  Deno.sleepSync(10);
 });
 
 canvas.on("event", (e) => {
@@ -286,11 +253,44 @@ canvas.on("event", (e) => {
   }
   if (e.type == "key_down") {
     // Space
-    if (e.keycode == 32) {
+    if (e.keycode == 32 && !gameOver) {
       intro = false;
       is_space = true;
     }
   }
 });
+
+canvas.clear();
+
+canvas.copy(BgScreenTexture, { x: 0, y: 0, width: 400, height: 800 }, {
+  x: 0,
+  y: 0,
+  width: 400,
+  height: 800,
+});
+
+const height = Math.floor(170 / 3) - 25;
+
+canvas.renderFont(font, "flappybird!", {
+  blended: { color: { r: 255, g: 255, b: 255, a: 255 } },
+}, {
+  x: (400 / 2) - 130,
+  y: (800 / 2) - (2 * height),
+  width: Math.floor(770 / 5),
+  height,
+});
+
+const width = Math.floor(770 / 3);
+
+canvas.renderFont(font, "Press Space to start", {
+  blended: { color: { r: 255, g: 255, b: 255, a: 255 } },
+}, {
+  x: (400 / 2) - 130,
+  y: (800 / 2) - height,
+  width,
+  height,
+});
+
+canvas.present();
 
 await canvas.start();
