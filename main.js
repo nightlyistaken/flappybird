03c7bd5c0f613aeb65b689947cfdc4dbbedb0746
Divy Srivastava <dj.srivastava23@gmail.com>
2021-08-25 21:23:45 +0530

    chore: upgrade to deno_sdl2 0.1-alpha.6

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Canvas } from "https://deno.land/x/sdl2@0.1-alpha.5/src/canvas.ts";
+import { Canvas } from "https://deno.land/x/sdl2@0.1-alpha.6/src/canvas.ts";
 
 const canvas = new Canvas({
   title: "Flappy Bird in Deno 🐦",
@@ -164,155 +164,6 @@ const pipeTextureDown = canvas.createTextureFromSurface(pipeSurfaceDown);
 
 const bird = new Player();
 
-canvas.on("draw", () => {
-  if (intro) {
-    return;
-  }
-
-  canvas.copy(BgScreenTexture, { x: 0, y: 0, width: 400, height: 800 }, {
-    x: 0,
-    y: 0,
-    width: 400,
-    height: 800,
-  });
-
-  for (let idx = 0; idx < upperPipes.length; idx++) {
-    if (
-      checkCollision(
-        bird.x,
-        bird.y,
-        34,
-        24,
-        upperPipes[idx].x,
-        UPPER_PIPE_Y,
-        PIPE_WIDTH,
-        upperPipes[idx].height,
-      ) ||
-      checkCollision(
-        bird.x,
-        bird.y,
-        34,
-        24,
-        lowerPipes[idx].x,
-        LOWER_PIPE_Y_BASE - lowerPipes[idx].height,
-        PIPE_WIDTH,
-        lowerPipes[idx].height,
-      )
-    ) {
-      // Only runs once
-      if (!gameOver) {
-        gameOver = true;
-        canvas.playMusic(
-          "./audio/game_over.wav",
-        );
-        canvas.present();
-      }
-    }
-    if (
-      checkCollision(
-        bird.x + 50 / 2,
-        bird.y,
-        0,
-        50,
-        upperPipes[idx].x + PIPE_WIDTH / 2,
-        upperPipes[idx].height,
-        0,
-        800 - upperPipes[idx].height - lowerPipes[idx].height,
-      )
-    ) {
-      score_value += 1;
-      const score_effects = ["scored_1.wav", "scored_2.wav"];
-      canvas.playMusic(
-        "./audio/" + score_effects[Math.floor(Math.random() * 2)],
-      );
-    }
-
-    // Debug:
-    // canvas.fillRect(playerX + 50 / 2, playerY, 0, 50)
-    // canvas.fillRect(upperPipes[idx].x + PIPE_WIDTH / 2, upperPipes[idx].height, 0, 800 - upperPipes[idx].height - lowerPipes[idx].height);
-
-    // Pipes
-    canvas.copy(pipeTextureDown, { x: 0, y: 0, width: 52, height: 320 }, {
-      x: upperPipes[idx].x,
-      y: UPPER_PIPE_Y,
-      width: PIPE_WIDTH,
-      height: upperPipes[idx].height,
-    });
-    canvas.copy(pipeTextureUp, { x: 0, y: 0, width: 52, height: 320 }, {
-      x: lowerPipes[idx].x,
-      y: LOWER_PIPE_Y_BASE - lowerPipes[idx].height,
-      width: PIPE_WIDTH,
-      height: lowerPipes[idx].height,
-    });
-    if (!gameOver) {
-      bird.render();
-
-      upperPipes[idx].x -= 1;
-      lowerPipes[idx].x -= 1;
-      if (upperPipes[idx].x <= -PIPE_WIDTH) {
-        upperPipes[idx].x = 800 + PIPE_WIDTH;
-        upperPipes[idx].height = getRandomInt(100, 190);
-        lowerPipes[idx].x = 800 + PIPE_WIDTH;
-        lowerPipes[idx].height = 800 - upperPipes[idx].height - GAP;
-      }
-
-      if (bird.y >= 800 - 50) {
-        gameOver = true;
-
-        canvas.playMusic(
-          "./audio/game_over.wav",
-        );
-      }
-    } else {
-      bird.die();
-      bird.render();
-    }
-  }
-
-  canvas.renderFont(font, score_value.toString(), {
-    blended: { color: { r: 255, g: 255, b: 255, a: 255 } },
-  }, {
-    x: 10,
-    y: -20,
-  });
-
-  // Game physics
-  if (is_space) {
-    bird.y -= 50;
-    is_space = false;
-  } else {
-    // Give player downwards acceleration
-    bird.y += gravity;
-  }
-  if (bird.y >= 800 - 50) {
-    bird.y = 800 - 50;
-  }
-  canvas.present();
-  Deno.sleepSync(fps);
-});
-
-canvas.on("event", (e) => {
-  if (e.type == "quit") {
-    canvas.quit();
-  }
-  if (e.type == "mouse_button_down" && e.button == 1 && !gameOver) {
-    // Left click
-    intro = false;
-    is_space = true;
-  }
-  if (e.type == "key_down") {
-    // Space
-    if (e.keycode == 32 && !gameOver) {
-      intro = false;
-      is_space = true;
-    }
-    if (e.keycode == 114 && gameOver) {
-      intro = true;
-      gameOver = false;
-    }
-  }
-});
-
 canvas.clear();
 
 canvas.copy(BgScreenTexture, { x: 0, y: 0, width: 400, height: 800 }, {
@@ -330,4 +181,156 @@ canvas.copy(startTexture, { x: 0, y: 0, width: 400, height: 800 }, {
 
 canvas.present();
 
-await canvas.start();
+for await (const event of canvas) {
+  switch (event.type) {
+    case "draw":
+      if (intro) {
+        break;
+      }
+
+      canvas.copy(BgScreenTexture, { x: 0, y: 0, width: 400, height: 800 }, {
+        x: 0,
+        y: 0,
+        width: 400,
+        height: 800,
+      });
+
+      for (let idx = 0; idx < upperPipes.length; idx++) {
+        if (
+          checkCollision(
+            bird.x,
+            bird.y,
+            34,
+            24,
+            upperPipes[idx].x,
+            UPPER_PIPE_Y,
+            PIPE_WIDTH,
+            upperPipes[idx].height,
+          ) ||
+          checkCollision(
+            bird.x,
+            bird.y,
+            34,
+            24,
+            lowerPipes[idx].x,
+            LOWER_PIPE_Y_BASE - lowerPipes[idx].height,
+            PIPE_WIDTH,
+            lowerPipes[idx].height,
+          )
+        ) {
+          // Only runs once
+          if (!gameOver) {
+            gameOver = true;
+            canvas.playMusic(
+              "./audio/game_over.wav",
+            );
+            canvas.present();
+          }
+        }
+        if (
+          checkCollision(
+            bird.x + 50 / 2,
+            bird.y,
+            0,
+            50,
+            upperPipes[idx].x + PIPE_WIDTH / 2,
+            upperPipes[idx].height,
+            0,
+            800 - upperPipes[idx].height - lowerPipes[idx].height,
+          )
+        ) {
+          score_value += 1;
+          const score_effects = ["scored_1.wav", "scored_2.wav"];
+          canvas.playMusic(
+            "./audio/" + score_effects[Math.floor(Math.random() * 2)],
+          );
+        }
+
+        // Debug:
+        // canvas.fillRect(playerX + 50 / 2, playerY, 0, 50)
+        // canvas.fillRect(upperPipes[idx].x + PIPE_WIDTH / 2, upperPipes[idx].height, 0, 800 - upperPipes[idx].height - lowerPipes[idx].height);
+
+        // Pipes
+        canvas.copy(pipeTextureDown, { x: 0, y: 0, width: 52, height: 320 }, {
+          x: upperPipes[idx].x,
+          y: UPPER_PIPE_Y,
+          width: PIPE_WIDTH,
+          height: upperPipes[idx].height,
+        });
+        canvas.copy(pipeTextureUp, { x: 0, y: 0, width: 52, height: 320 }, {
+          x: lowerPipes[idx].x,
+          y: LOWER_PIPE_Y_BASE - lowerPipes[idx].height,
+          width: PIPE_WIDTH,
+          height: lowerPipes[idx].height,
+        });
+        if (!gameOver) {
+          bird.render();
+
+          upperPipes[idx].x -= 1;
+          lowerPipes[idx].x -= 1;
+          if (upperPipes[idx].x <= -PIPE_WIDTH) {
+            upperPipes[idx].x = 800 + PIPE_WIDTH;
+            upperPipes[idx].height = getRandomInt(100, 190);
+            lowerPipes[idx].x = 800 + PIPE_WIDTH;
+            lowerPipes[idx].height = 800 - upperPipes[idx].height - GAP;
+          }
+
+          if (bird.y >= 800 - 50) {
+            gameOver = true;
+
+            canvas.playMusic(
+              "./audio/game_over.wav",
+            );
+          }
+        } else {
+          bird.die();
+          bird.render();
+        }
+      }
+
+      canvas.renderFont(font, score_value.toString(), {
+        blended: { color: { r: 255, g: 255, b: 255, a: 255 } },
+      }, {
+        x: 10,
+        y: -20,
+      });
+
+      // Game physics
+      if (is_space) {
+        bird.y -= 50;
+        is_space = false;
+      } else {
+        // Give player downwards acceleration
+        bird.y += gravity;
+      }
+      if (bird.y >= 800 - 50) {
+        bird.y = 800 - 50;
+      }
+      canvas.present();
+      Deno.sleepSync(fps);
+      break;
+    case "quit":
+      canvas.quit();
+      break;
+    case "key_down":
+      // Space
+      if (event.keycode == 32 && !gameOver) {
+        intro = false;
+        is_space = true;
+      }
+      if (event.keycode == 114 && gameOver) {
+        intro = true;
+        gameOver = false;
+      }
+      break;
+    case "mouse_button_down":
+      if (event.button == 1 && !gameOver) {
+        // Left click
+        intro = false;
+        is_space = true;
+      }
+      break;
+    default:
+      break;
+  }
+}
